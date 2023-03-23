6ad5dd03c0c9ebf0d8f6250362e0d94043cac6e4
plaiaundi <plaiaundi@HZ321162.H012108.NET>
2023-03-23 10:49:21 +0100

    fecha add

--- src/main/java/modelo/Usuario.java
+++ src/main/java/modelo/Usuario.java
@@ -1,11 +1,20 @@
 package modelo;
 
+import java.util.Date;
+
 public class Usuario {
 	private int id;
 	private String nombre;
 	private String password;
+	private Date fecha_login;
 	
 	
+	public Date getFecha_login() {
+		return fecha_login;
+	}
+	public void setFecha_login(Date fecha_login) {
+		this.fecha_login = fecha_login;
+	}
 	public String getPassword() {
 		return password;
 	}
@@ -28,15 +37,17 @@ public class Usuario {
 	public Usuario() {
 		super();
 	}
-	public Usuario(int id, String nombre, String password) {
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nombre=" + nombre + ", password=" + password + ", fecha_login=" + fecha_login
+				+ "]";
+	}
+	public Usuario(int id, String nombre, String password, Date fecha_login) {
 		super();
 		this.id = id;
 		this.nombre = nombre;
 		this.password = password;
-	}
-	@Override
-	public String toString() {
-		return "Usuario [id=" + id + ", nombre=" + nombre + ", password=" + password + "]";
+		this.fecha_login = fecha_login;
 	}
 	
 
