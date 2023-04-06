0d5df3daf89d577f0e1fdf08b6cb8d90a10e4b03
GerardoLA <119864519+GerardoLA@users.noreply.github.com>
2023-04-06 08:48:08 +0200

    Update Usuario.java

--- src/main/java/modelo/Usuario.java
+++ src/main/java/modelo/Usuario.java
@@ -5,22 +5,22 @@ import java.util.Date;
 public class Usuario {
 	private int id;
 	private String nombre;
-	private String contrasena;
+	private String password;
 	private Date fecha_login;
+
 	
-	
-	public Date getFecha_login() {
-		return fecha_login;
+	public Usuario() {
+		super();
 	}
-	public void setFecha_login(Date fecha_login) {
+	public Usuario(int id, String nombre, String password, Date fecha_login) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.password = password;
 		this.fecha_login = fecha_login;
+		
 	}
-	public String getContrasena() {
-		return contrasena;
-	}
-	public void setContrasena(String contrasena) {
-		this.contrasena = contrasena;
-	}
+	
 	public int getId() {
 		return id;
 	}
@@ -30,18 +30,36 @@ public class Usuario {
 	public String getNombre() {
 		return nombre;
 	}
+	
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 	
-	public Usuario() {
-		super();
+	public String getPassword() {
+		return password;
 	}
+	public void setPassword(String password) {
+		this.password = password;
+	
+	}
+	
+	public Date getFecha_login() {
+		return fecha_login;
+	}
+	public void setFecha_login(Date fecha_login) {
+		this.fecha_login = fecha_login;
+	}
+	
 	@Override
 	public String toString() {
-		return "Usuario [id=" + id + ", nombre=" + nombre + ", password=" + contrasena + ", fecha_login=" + fecha_login
-				+ "]";
+		return "Usuario [id=" + id + ", nombre=" + nombre + ", password=" + password + ", fecha_login=" + fecha_login
+				 + "]";
 	}
+
+	
+
+
 	
 
 }
+
